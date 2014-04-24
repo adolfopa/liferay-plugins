42c77d3e2fb81bdf609810e862f67cb5e165afd3
Brian Chan <brian.chan@liferay.com>
2014-04-23 21:09:45 -0700

    LPS-43880 Auto SF

--- portlets/kaleo-forms-portlet/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/forms/util/KaleoFormsUtil.java
+++ portlets/kaleo-forms-portlet/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/forms/util/KaleoFormsUtil.java
@@ -14,10 +14,10 @@
 
 package com.liferay.portal.workflow.kaleo.forms.util;
 
-import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
