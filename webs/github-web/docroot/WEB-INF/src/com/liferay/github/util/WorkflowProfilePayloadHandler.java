2639398bbe8fd447777c7b96cff9738ed35c6142
Peter Shin <peter.shin@liferay.com>
2014-05-09 07:51:43 -0700

    GitHub Git commands

--- webs/github-web/docroot/WEB-INF/src/com/liferay/github/util/WorkflowProfilePayloadHandler.java
+++ webs/github-web/docroot/WEB-INF/src/com/liferay/github/util/WorkflowProfilePayloadHandler.java
@@ -14,6 +14,8 @@
 
 package com.liferay.github.util;
 
+import java.io.File;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -69,7 +71,43 @@ public class WorkflowProfilePayloadHandler {
 	}
 
 	public void syncProfileBranch() {
+		File file = new File(
+			_PATHNAME_WORKFLOW_DIR + "/profiles/" + _profileName + "/plugins");
+
+		String excludePattern =
+			"build." + System.getenv("USERNAME") + ".properties";
+
+		WorkflowProcessUtil.execute(
+			"git clean -d -f -q -x -e .ivy -e " + excludePattern, file);
+
+		WorkflowProcessUtil.execute("git reset --hard HEAD", file);
+
+		WorkflowProcessUtil.execute("git fetch upstream", file);
+
+		WorkflowProcessUtil.execute(
+			"git checkout upstream/" + _PROFIE_BASE_BRANCH, file);
+
+		String workflowBranch = "workflow-" + _profileName;
+
+		Object[] returnValue = WorkflowProcessUtil.execute(
+			"git branch --list " + workflowBranch, file);
+
+		int exitCode = (Integer)returnValue[0];
+		String output = (String)returnValue[1];
+
+		if ((exitCode == 0) && (output.length() > 0)) {
+			WorkflowProcessUtil.execute(
+				"git branch -D " + workflowBranch, file);
+		}
+
+		WorkflowProcessUtil.execute("git checkout -b " + workflowBranch, file);
+
+		WorkflowProcessUtil.execute("git push origin " + workflowBranch, file);
 	}
+
+	private static final String _PATHNAME_WORKFLOW_DIR = "/var/workflow";
+
+	private static final String _PROFIE_BASE_BRANCH = "ee-6.1.x";
 
 	private List<String> _messages;
 	private JSONObject _payloadJSONObject;
