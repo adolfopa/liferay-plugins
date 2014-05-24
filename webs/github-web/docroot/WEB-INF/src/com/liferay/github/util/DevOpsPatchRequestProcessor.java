8b2a9358c2284ef18eafe128382613d14e879b63
Peter Shin <peter.shin@liferay.com>
2014-05-24 13:24:50 -0700

    LRDCOM-9612

--- webs/github-web/docroot/WEB-INF/src/com/liferay/github/util/DevOpsPatchRequestProcessor.java
+++ webs/github-web/docroot/WEB-INF/src/com/liferay/github/util/DevOpsPatchRequestProcessor.java
@@ -52,7 +52,7 @@ public class DevOpsPatchRequestProcessor {
 			JSONObject payloadJSONObject = _payloadJSONObjects.poll();
 
 			if (payloadJSONObject != null) {
-				process(payloadJSONObject);
+				doProcess(payloadJSONObject);
 			}
 		}
 		finally {
@@ -60,10 +60,85 @@ public class DevOpsPatchRequestProcessor {
 		}
 	}
 
-	protected void process(JSONObject payloadJSONObject) {
+	protected void doProcess(JSONObject payloadJSONObject) {
+		try {
+			initProfileGitRepository(payloadJSONObject);
+
+			String[] sha1Hashes = getSHA1Hashes(payloadJSONObject);
+
+			if (hasProtectedPath(payloadJSONObject, sha1Hashes)) {
+				return;
+			}
+
+			if (hasMergeConflict(payloadJSONObject, sha1Hashes)) {
+				return;
+			}
+
+			if (hasCompileError(payloadJSONObject, sha1Hashes)) {
+				return;
+			}
+
+			updateProfileGitRepository(payloadJSONObject, sha1Hashes);
+
+			installPatch(payloadJSONObject, sha1Hashes);
+
+			updateJIRAIssue(payloadJSONObject, sha1Hashes);
+
+			updatePullRequest(payloadJSONObject, sha1Hashes);
+		}
+		finally {
+			sendEmail(payloadJSONObject);
+		}
+	}
+
+	protected String[] getSHA1Hashes(JSONObject payloadJSONObject) {
+		String sha1HashParent = "";
+		String sha1HashHead = "";
+
+		return new String[] {sha1HashParent, sha1HashHead};
+	}
+
+	protected boolean hasCompileError(
+		JSONObject payloadJSONObject, String[] sha1Hashes) {
+
+		return false;
+	}
+
+	protected boolean hasMergeConflict(
+		JSONObject payloadJSONObject, String[] sha1Hashes) {
+
+		return false;
+	}
+
+	protected boolean hasProtectedPath(
+		JSONObject payloadJSONObject, String[] sha1Hashes) {
+
+		return false;
+	}
+
+	protected void initProfileGitRepository(JSONObject payloadJSONObject) {
 		_devOpsGitHubRequestProcessor.updateProfileGitRepository(_profileName);
+	}
+
+	protected void installPatch(
+		JSONObject payloadJSONObject, String[] sha1Hashes) {
 
 		_devOpsGitHubRequestProcessor.updatePeekGitRepository(_profileName);
+	}
+
+	protected void sendEmail(JSONObject payloadJSONObject) {
+	}
+
+	protected void updateJIRAIssue(
+		JSONObject payloadJSONObject, String[] sha1Hashes) {
+	}
+
+	protected void updateProfileGitRepository(
+		JSONObject payloadJSONObject, String[] sha1Hashes) {
+	}
+
+	protected void updatePullRequest(
+		JSONObject payloadJSONObject, String[] sha1Hashes) {
 	}
 
 	private DevOpsGitHubRequestProcessor _devOpsGitHubRequestProcessor;
