634a644fca3e38e8e591c9fb02320408078d1fb9
rachaelkoestartyo <rachael.koestartyo@liferay.com>
2014-06-02 14:57:45 -0700

    LRDCOM-9653 Base message listener

---
+++ portlets/osb-metrics-portlet/docroot/WEB-INF/src/com/liferay/osbmetrics/messaging/BaseReportsTeamMetricsMessageListener.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osbmetrics.messaging;
+
+import com.liferay.osbmetrics.util.OSBMetricsUtil;
+import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
+
+import java.text.Format;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Rachael Koestartyo
+ */
+public abstract class BaseReportsTeamMetricsMessageListener
+	extends BaseMessageListener {
+
+	@Override
+	protected void doReceive(Message message) throws Exception {
+		OSBMetricsUtil.addReportEntry(
+			"OSB_TeamMetricsByMonthAndSupportRegion", getEntryReportName(),
+			getEmailAddresses(), getParameterMap());
+	}
+
+	protected abstract String getEmailAddresses();
+
+	protected String getEntryReportName() {
+		return "[" + getSupportRegion() + "] Team Metrics";
+	}
+
+	protected Map<String, String> getParameterMap() {
+		Map<String, String> reportParameters = new HashMap<String, String>();
+
+		Calendar calendar = Calendar.getInstance();
+
+		calendar.add(Calendar.MONTH, -1);
+
+		calendar.set(Calendar.DATE, calendar.getActualMaximum(Calendar.DATE));
+
+		Format format = FastDateFormatFactoryUtil.getSimpleDateFormat(
+			"yyyy-MM-dd");
+
+		reportParameters.put("endDate", format.format(calendar.getTime()));
+
+		calendar.set(Calendar.DATE, 1);
+
+		reportParameters.put("startDate", format.format(calendar.getTime()));
+		reportParameters.put("supportRegion", getSupportRegion());
+
+		return reportParameters;
+	}
+
+	protected abstract String getSupportRegion();
+
+}
