7af9e10b10837175f4b6b2ec8c6714646099035a
rachaelkoestartyo <rachael.koestartyo@liferay.com>
2014-06-03 14:42:57 -0700

    LRDCOM-9652 Base message listener

---
+++ portlets/osb-metrics-portlet/docroot/WEB-INF/src/com/liferay/osbmetrics/messaging/BaseEngineerReportMetricsMessageListener.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osbmetrics.messaging;
+
+import java.util.Map;
+
+/**
+ * @author Rachael Koestartyo
+ */
+public abstract class BaseEngineerReportMetricsMessageListener
+	extends BaseReportMetricsMessageListener {
+
+	@Override
+	protected String getEntryReportName() {
+		return "[" + getSupportTeam() + "] Engineer Metrics";
+	}
+
+	@Override
+	protected Map<String, String> getParameterMap() {
+		Map<String, String> parameterMap = super.getParameterMap();
+
+		parameterMap.put("supportTeam", getSupportTeam());
+
+		return parameterMap;
+	}
+
+	@Override
+	protected String getReportDefinitionName() {
+		return "OSB_EngineerMetricsByMonthAndSupportTeam";
+	}
+
+	protected abstract String getSupportTeam();
+
+}
