20904a10c54b4569399e9f53e054dd5fbc07a71f
Peter Shin <peter.shin@liferay.com>
2014-06-13 08:08:07 -0700

    LRDCOM-9959 Copy GitHubPropsUtil

--- webs/github-web/docroot/WEB-INF/src/com/liferay/github/util/DevOpsPropsUtil.java
+++ webs/github-web/docroot/WEB-INF/src/com/liferay/github/util/DevOpsPropsUtil.java
@@ -14,14 +14,44 @@
 
 package com.liferay.github.util;
 
+import java.io.IOException;
+
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * @author Brian Wing Shun Chan
- * @author Peter Shin
  */
-public class DevOpsPropsUtil {
+public class GitHubPropsUtil {
 
 	public static String get(String key) {
-		return "";
+		return _instance._get(key);
 	}
+
+	private GitHubPropsUtil() {
+		try {
+			Class<?> clazz = getClass();
+
+			ClassLoader classLoader = clazz.getClassLoader();
+
+			_properties.load(
+				classLoader.getResourceAsStream("/github.properties"));
+		}
+		catch (IOException ioe) {
+			_log.error(ioe, ioe);
+		}
+	}
+
+	private String _get(String key) {
+		return _properties.getProperty(key);
+	}
+
+	private static Log _log = LogFactory.getLog(GitHubPropsUtil.class);
+
+	private static GitHubPropsUtil _instance = new GitHubPropsUtil();
+
+	private Properties _properties = new Properties();
 
 }
