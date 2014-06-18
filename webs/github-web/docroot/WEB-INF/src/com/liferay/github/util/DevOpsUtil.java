c1c44eff6998d0b8d6cd8c2aa37c4a2e714af9f9
Peter Shin <peter.shin@liferay.com>
2014-06-17 21:05:40 -0500

    LRDCOM-9998

--- webs/github-web/docroot/WEB-INF/src/com/liferay/github/util/DevOpsUtil.java
+++ webs/github-web/docroot/WEB-INF/src/com/liferay/github/util/DevOpsUtil.java
@@ -22,25 +22,133 @@ import org.json.JSONObject;
  */
 public class DevOpsUtil {
 
-	public static void closePullRequest(JSONObject payloadJSONObject) {
+	public static JSONObject closePullRequest(JSONObject payloadJSONObject)
+		throws Exception {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(_GITHUB_API_URL);
+		sb.append("/repos/");
+
+		JSONObject pullRequestJSONObject = payloadJSONObject.getJSONObject(
+			"pull_request");
+
+		JSONObject baseJSONObject = pullRequestJSONObject.getJSONObject("base");
+
+		JSONObject repoJSONObject = baseJSONObject.getJSONObject("repo");
+
+		sb.append(repoJSONObject.getString("full_name"));
+		sb.append("/pulls/");
+		sb.append(pullRequestJSONObject.getInt("number"));
+
+		JSONObject dataJSONObject = new JSONObject();
+
+		dataJSONObject.put("state", "closed");
+
+		return executePost(sb.toString(), dataJSONObject);
 	}
 
-	public static String getProfileName(JSONObject payloadJSONObject) {
+	public static String getProfileName(JSONObject payloadJSONObject)
+		throws Exception {
+
+		JSONObject pullRequestJSONObject = payloadJSONObject.getJSONObject(
+			"pull_request");
+
+		JSONObject baseJSONObject = pullRequestJSONObject.getJSONObject("base");
+
+		String ref = baseJSONObject.getString("ref");
+
+		String profileNames = DevOpsPropsUtil.get("profile.names");
+
+		for (String profileName : profileNames.split(",")) {
+			if (ref.equals("devops-" + profileName)) {
+				return profileName;
+			}
+
+			String pluginsGitBranch = DevOpsPeekPropsUtil.get(
+				profileName, "plugins.git.branch");
+
+			if (ref.equals(pluginsGitBranch)) {
+				return profileName;
+			}
+		}
+
 		return "";
 	}
 
-	public static boolean isValidAction(JSONObject payloadJSONObject) {
-		return true;
+	public static boolean isValidAction(JSONObject payloadJSONObject)
+		throws Exception {
+
+		JSONObject senderJSONObject = payloadJSONObject.getJSONObject("sender");
+
+		String action = payloadJSONObject.getString("action");
+		String login = senderJSONObject.getString("login");
+
+		if (action.equals("reopened") &&
+			login.equals(DevOpsPropsUtil.get("profile.github.user.login"))) {
+
+			return true;
+		}
+		else if (action.equals("opened")) {
+			return true;
+		}
+
+		return false;
 	}
 
-	public static boolean isValidDeveloper(
-		JSONObject payloadJSONObject, String profileName) {
+	public static boolean isValidLogin(
+			JSONObject payloadJSONObject, String profileName)
+		throws Exception {
 
-		return true;
+		JSONObject pullRequestJSONObject = payloadJSONObject.getJSONObject(
+			"pull_request");
+
+		JSONObject userJSONObject = pullRequestJSONObject.getJSONObject("user");
+
+		String gitHubUserLogins = DevOpsPropsUtil.get(
+			"profile." + profileName + ".github.user.logins");
+
+		for (String gitHubUserLogin : gitHubUserLogins.split(",")) {
+			if (gitHubUserLogin.equals(userJSONObject.getString("login"))) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
-	public static void postPullRequestComment(
-		JSONObject payloadJSONObject, String comment) {
+	public static JSONObject postPullRequestComment(
+			JSONObject payloadJSONObject, String comment)
+		throws Exception {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(_GITHUB_API_URL);
+		sb.append("/repos/");
+
+		JSONObject pullRequestJSONObject = payloadJSONObject.getJSONObject(
+			"pull_request");
+
+		JSONObject baseJSONObject = pullRequestJSONObject.getJSONObject("base");
+
+		JSONObject repoJSONObject = baseJSONObject.getJSONObject("repo");
+
+		sb.append(repoJSONObject.getString("full_name"));
+		sb.append("/issues/");
+		sb.append(pullRequestJSONObject.getInt("number"));
+		sb.append("/comments");
+
+		JSONObject dataJSONObject = new JSONObject();
+
+		dataJSONObject.put("body", comment);
+
+		return executePost(sb.toString(), dataJSONObject);
 	}
+
+	protected static JSONObject executePost(String url, JSONObject jsonObject) {
+		return null;
+	}
+
+	private static final String _GITHUB_API_URL = "https://api.github.com";
 
 }
