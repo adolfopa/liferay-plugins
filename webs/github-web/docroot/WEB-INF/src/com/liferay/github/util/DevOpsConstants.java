ca5c8a414128274b7bff0a2418ea46e9693fa776
Peter Shin <peter.shin@liferay.com>
2014-06-18 12:50:26 -0500

    LRDCOM-9984

---
+++ webs/github-web/docroot/WEB-INF/src/com/liferay/github/util/DevOpsConstants.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.github.util;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @author Peter Shin
+ */
+public class DevOpsConstants {
+
+	public static final String DEV_OPS_DIR_NAME = "/tmp/devops";
+
+	public static final String GITHUB_API_URL = "https://api.github.com";
+
+	public static final String LIFERAY_ORIGINAL_DIR_NAME =
+		"/opt/java/liferay.original";
+
+	public static final String PEEK_GIT_REPOSITORY_DIR_NAME = "/var/peek/repo";
+
+	public static final String PEEK_PLUGINS_GIT_REPOSITORY_DIR_NAME =
+		"/var/peek/builder-dependencies/plugins";
+
+}
