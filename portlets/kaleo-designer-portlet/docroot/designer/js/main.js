e45dfb0524c4bf783e77047df1e5d8afbe412c9a
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-11-10 16:54:06 -0800

    LPS-49485 - Source formatting

--- portlets/kaleo-designer-portlet/docroot/designer/js/main.js
+++ portlets/kaleo-designer-portlet/docroot/designer/js/main.js
@@ -366,10 +366,10 @@ AUI.add(
 
 						formatted += item + STR_CHAR_CRLF;
 
-						if (item.indexOf(STR_CDATA_OPEN) != -1) {
+						if (item.indexOf(STR_CDATA_OPEN) > -1) {
 							inCDATA = true;
 						}
-						else if (item.indexOf(STR_CDATA_CLOSE) != -1) {
+						else if (item.indexOf(STR_CDATA_CLOSE) > -1) {
 							inCDATA = false;
 						}
 
