bde982c6143cc6fe0c5da0a41f9f3f7281d6ad7e
Brian Chan <brian.chan@liferay.com>
2015-01-09 10:14:15 -0800

    LPS-52255 SF

--- hooks/documentum-hook/docroot/WEB-INF/src/com/liferay/documentum/repository/DocumentumAction.java
+++ hooks/documentum-hook/docroot/WEB-INF/src/com/liferay/documentum/repository/DocumentumAction.java
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.exception.PortalException;
  * @author Ivan Zaera
  */
 public interface DocumentumAction<T> {
+
 	public T run(IDfSession iDfSession) throws DfException, PortalException;
 
 }
